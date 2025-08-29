e4eea694cb55b6892b133823e41d4596610cb9cf
lqc <1521028580@qq.com>
2025-08-29 11:32:56 +0800

    'finish'

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const router = createRouter({
         {path:'paycallback',component:()=>import('@/views/pay/PayBack.vue')},
         {path:'member',component:()=>import('@/views/Member/index.vue'),
           children:[
-            {path:'user',component:()=>import('@/views/Member/components/UserInfo.vue')},
+            {path:'',component:()=>import('@/views/Member/components/UserInfo.vue')},
             {path:'order',component:()=>import('@/views/Member/components/UserOrder.vue')}
           ]
         },
